29b63b0cdaa8e1b034df53a2d30c32f1d93c3ce2
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-01-19 11:09:58 -0500

    Initial Commit

---
+++ AreaAndPerimeterCalculator/AreaAndPerimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimeterCalculatorApp.swift
+//  AreaAndPerimeterCalculator
+//
+//  Created by Emanuel Gross von Trockau on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
